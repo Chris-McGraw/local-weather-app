7f14bac9fc48da436a250ec5fbd864b26aa0fcf9
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-01-18 17:56:28 -0500

    loading-effect-update-1

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
-$(document).ready(function() { 
-  if (navigator.geolocation) {navigator.geolocation.getCurrentPosition(function(position) {   
+$(document).ready(function() {
+  $("#current-location").toggleClass("loading-location");
+  $("#current-weather").toggleClass("loading-weather");
+  $("#weather-icon").toggleClass("loading-img");
+  $("#current-temp-far").toggleClass("loading-temp");
+});
+
+
+$(window).load(function(){
+  if (navigator.geolocation) {navigator.geolocation.getCurrentPosition(function(position) {
     var lat = Math.round(position.coords.latitude * 100) / 100;
     var long = Math.round(position.coords.longitude * 100) / 100;
     $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + long, function(json) {
-      $("#current-location").html(json.name);
+
+      $("#current-location").toggleClass("loading-location");
+
+      $("#current-location").html(json.name + "-");
       $("#current-country").html(json.sys.country);
-      
+
       if(json.weather[0].description === "few clouds") {
         json.weather[0].description = "partly cloudy";
       }
-      
+
       currentWeather = json.weather[0].description.split(" ");
-      
+
       for (var i = 0; i < currentWeather.length; i++) {
         currentWeather[i] = currentWeather[i].charAt(0).toUpperCase() + currentWeather[i].slice(1);
       }
-      
+
       currentWeather = currentWeather.join(" ");
-      
+
+      $("#current-weather").toggleClass("loading-weather");
+      $("#weather-icon").toggleClass("loading-img");
+      $("#current-temp-far").toggleClass("loading-temp");
+
       $("#current-weather").html(currentWeather);
-      $("#weather-icon").html("<img src='" + json.weather[0].icon + "'>"); 
-      
+      $("#weather-icon").html("<img src='" + json.weather[0].icon + "'>");
+
       $("#current-temp-cel").html(json.main.temp + "&deg;C");
       $("#current-temp-far").html(Math.round(json.main.temp * 1.8 + 32) + "&deg;F");
-      
+
       $("#current-temp-far").on("click", function() {
         $("#current-temp-far").toggle(350);
         $("#current-temp-cel").toggle(350);
       });
-      
+
       $("#current-temp-cel").on("click", function() {
         $("#current-temp-cel").toggle(350);
         $("#current-temp-far").toggle(350);
       });
-      
     });
-  });  
+  });
   }
 });
